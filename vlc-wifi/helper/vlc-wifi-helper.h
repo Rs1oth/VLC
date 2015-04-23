66dd82895fe74ae645a93eb48b844ed4c166d940
RyanAckerman <rea9@njit.edu>
2015-04-22 21:37:08 -0400

    Create vlc-wifi-helper.h

---
+++ vlc-wifi/helper/vlc-wifi-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef VLC_WIFI_HELPER_H
+#define VLC_WIFI_HELPER_H
+
+#include "ns3/vlc-wifi.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* VLC_WIFI_HELPER_H */
+
